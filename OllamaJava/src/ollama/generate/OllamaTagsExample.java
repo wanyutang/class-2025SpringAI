3c1efe65d7d714f2fc74aaae75e6233f47b270e3
User <User@host.docker.internal>
2025-11-01 16:11:11 +0800

    ai

---
+++ OllamaJava/src/ollama/generate/OllamaTagsExample.java
@@ -0,0 +1,10 @@
+package ollama.generate;
+
+public class OllamaTagsExample {
+
+	public static void main(String[] args) {
+		// 利用 OKhttp + http://localhost:11434/api/tags
+		// 印出 model 名稱
+	}
+
+}
